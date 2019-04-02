ef9c4cedc1bb16e39e3ffc641d134da7b78f3e7e
43Clock <luiscarlosm2000@gmail.com>
2019-04-02 19:55:17 +0100

    Substituir

--- Projeto/main.c
+++ Projeto/main.c
@@ -61,6 +61,7 @@ ESTADO interface (ESTADO e) {
             }
             else printf("Ainda não começou o jogo !!!!\n\n");
             break;
+        case ':
 
 
     }
@@ -75,53 +76,3 @@ int main() {
         e = interface(e);
     }
 }
-/*
-    // estado inicial do tabuleiro. Inicio do jogo!
-    e.grelha[3][4] = VALOR_O;
-    e.grelha[4][3] = VALOR_O;
-    e.grelha[3][3] = VALOR_X;
-    e.grelha[4][4] = VALOR_X;
-    //e.grelha[2][2] = VALOR_X;
-
-    printf("\n");
-    printa(e);
-    printf("\n");
-
-
-    if (c == 'X') {
-        while (possivelJogar(e) == 1) {
-            e = jogarX(e);
-            printa(e);
-            printf("\nPontuação do jogador O: %d\n", conta(e, VALOR_O));
-            printf("Pontuação do jogador X: %d\n", conta(e, VALOR_X));
-            e = jogarO(e);
-            printa(e);
-            printf("\nPontuação do jogador O: %d\n", conta(e, VALOR_O));
-            printf("Pontuação do jogador X: %d\n", conta(e, VALOR_X));
-        }
-    }
-    else {
-        while (possivelJogar(e) == 1) {
-            e = jogarO(e);
-            printa(e);
-            printf("\nPontuação do jogador O: %d\n", conta(e, VALOR_O));
-            printf("Pontuação do jogador X: %d\n", conta(e, VALOR_X));
-            e = jogarX(e);
-            printa(e);
-            printf("\nPontuação do jogador O: %d\n", conta(e, VALOR_O));
-            printf("Pontuação do jogador X: %d\n", conta(e, VALOR_X));
-        }
-
-    }
-
-    if (conta (e,VALOR_X) > conta(e,VALOR_O)) printf("\nJogador X ganhou.\n");
-    if (conta (e,VALOR_X) < conta(e,VALOR_O)) printf("\nJogador O ganhou.\n");
-    if (conta (e,VALOR_X) > conta(e,VALOR_O)) printf("\nEmpate.\n");
-
-    //printf("\n");
-    //printa(e);
-    //printf("\n");
-
-
-    return 0;
-}*/
