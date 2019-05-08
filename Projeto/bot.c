bc963d5702dd1f4a84e2183630f55ac622a349ff
44Mozart <mariaqbarros28@gmail.com>
2019-05-08 11:21:27 +0100

    Toma. São mil

--- Projeto/bot.c
+++ Projeto/bot.c
@@ -4,7 +4,11 @@
 #include <time.h>
 #include "estado.h"
 #include "auxiliares.h"
-
+/**
+ *
+ * @param e - Estado atual do jogo
+ * @param p - Array com todas as posições possiveis para o jogador em questão
+ */
 void listaPosicoes (ESTADO e,POSICOES *p) {
     int i,j;
     POSICAO pos;
@@ -18,7 +22,13 @@ void listaPosicoes (ESTADO e,POSICOES *p) {
             }
 }
 
-
+/**
+ *
+ * @param e - Estado atual do jogo
+ * @param l - Linha onde vai jogar
+ * @param c - Coluna onde vai jogar
+ * @return - Estado depois da jogada
+ */
 ESTADO jogaBot (ESTADO e,int l, int c) {
     if (l - 1 >= 8 || c - 1 >= 8 || e.grelha[l - 1][c - 1] != VAZIA) {
         printf("Posição Invalida\n");
@@ -28,6 +38,12 @@ ESTADO jogaBot (ESTADO e,int l, int c) {
     return e;
 }
 
+/**
+ *
+ * @param e - Estado atual do jogo
+ * @param p - Posições possiveis para o bot jogar
+ * @return - Estado depois da jogada do bot
+ */
 ESTADO bot1 (ESTADO e,POSICOES *p) {
     int j = p->sp;
     int l,c,i,pontos = -100,tmp;
@@ -57,7 +73,12 @@ ESTADO bot1 (ESTADO e,POSICOES *p) {
 
     return e;
 }
-
+/**
+ * @brief - Matriz usada na heuristica do bot em que cada posição corresponde a uma pontuação
+ * @param l - Linha
+ * @param c - Coluna
+ * @return - Valor da posição
+ */
 int pontos_pos (int l,int c) {
     int matrizPontos[8][8] = { {25, -5, 14, 10, 10, 14, -5, 25},
                                {-5, -7, -4,  1,  1, -4, -7, -5},
@@ -70,6 +91,12 @@ int pontos_pos (int l,int c) {
     return matrizPontos[l-1][c-1];
 }
 
+/**
+ *
+ * @param e - Estado atual do jogo
+ * @param a - O jogador para o qual vai ser contado o numero de jogadas possiveis
+ * @return - Quantidade de jogadas possiveis para o jogador
+ */
 int conta_mob (ESTADO e,VALOR a) {
     int i,j,r = 1; //Ver o erro que dava quando r = 0;
     e.peca = a;
@@ -81,6 +108,18 @@ int conta_mob (ESTADO e,VALOR a) {
     return r;
 }
 
+/**
+ *
+ * @param e - Estado atual do jogo
+ * @param depth - O quão fundo o bot vai analisar as jogadas possiveis.
+ * @param original - A profundidade original
+ * @param alfa - Variavel usada para fazer com que o bot faça o menor numero de verificações possiveis
+ * @param beta - Variavel usada para fazer com que o bot faca o menor numero de verificacoes possiveis
+ * @param p - Posiçoes onde pode jogar
+ * @param bot - Valor do bot
+ * @param final - Posicao final
+ * @return
+ */
 int minmax (ESTADO e,int depth,int original,int alfa, int beta,POSICOES *p,VALOR bot,POSICAO *final) {
     VALOR v;
     ESTADO e_tmp;
@@ -135,6 +174,16 @@ int minmax (ESTADO e,int depth,int original,int alfa, int beta,POSICOES *p,VALOR
     }
 }
 
+/**
+ *
+ * @param e - Estado atual do jogo
+ * @param bot - Valor do bot
+ * @return
+ */
+
+
+// Falta return
+
 int pontos(ESTADO e,VALOR bot) {
     VALOR v;
     int i,j,h1 = 0,h2 = 0,h3 = 0;
@@ -149,7 +198,13 @@ int pontos(ESTADO e,VALOR bot) {
     h3 = (conta_mob(e,bot)-conta_mob(e,v))/(conta_mob(e,bot)+conta_mob(e,v));
     return h1+50*h2+100*h3;
 }
-
+/**
+ *
+ * @param e - Estado  atual do jogo
+ * @param c
+ * @param p
+ * @return
+ */
 ESTADO bot2 (ESTADO e,char c,POSICOES *p) {
     POSICAO final;
     int cd = c - '0';
