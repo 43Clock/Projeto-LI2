7e08960c7225d8eef5cd06a36d372dc90bb7652c
43Clock <48367937+43Clock@users.noreply.github.com>
2020-02-15 11:46:03 +0000

    Add files via upload

---
+++ aula1/tarefa3/cmake-build-debug/CMakeFiles/tarefa3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tarefa3.dir/main.c.o"
+  "tarefa3.pdb"
+  "tarefa3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tarefa3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
